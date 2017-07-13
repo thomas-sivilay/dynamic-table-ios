3a78b1100bed6b63864702809b8a96ffc0ecfbb1
Thomas Sivilay <thomas.sivilay@gmail.com>
2017-07-13 17:01:58 +0100

    Added view controller to playground

--- Dynamic Table/Dynamic Table/Playgrounds/UTests.playground/Contents.swift
+++ Dynamic Table/Dynamic Table/Playgrounds/UTests.playground/Contents.swift
@@ -1,5 +1,3 @@
-//: Playground - noun: a place where people can play
+import XCTest
 
-import UIKit
 
-var str = "Hello, playground"
--- Dynamic Table/Dynamic Table/Playgrounds/Views.playground/Contents.swift
+++ Dynamic Table/Dynamic Table/Playgrounds/Views.playground/Contents.swift
@@ -1,5 +1,58 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
+import PlaygroundSupport
 
-var str = "Hello, playground"
+typealias JSON = [String: Any]
+
+let style: JSON = [
+    "text": [ "size": 10],
+    "title": [ "size": 16]
+]
+
+let json: JSON = [
+    "title": "Hello!",
+    "text": "Product A",
+]
+
+
+
+final class ViewController: UIViewController {
+    
+    private var collectionView: UICollectionView {
+        didSet {
+            collectionView.delegate = self
+            collectionView.dataSource = self
+        }
+    }
+    
+    init() {
+        self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+let vc = ViewController()
+PlaygroundPage.current.liveView = vc
