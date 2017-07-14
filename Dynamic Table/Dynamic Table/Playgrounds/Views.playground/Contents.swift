20bf3d9c351285547a56ab2100a594ac2e770ea5
Thomas Sivilay <thomas.sivilay@gmail.com>
2017-07-14 10:25:12 +0100

    Added cell

--- Dynamic Table/Dynamic Table/Playgrounds/Views.playground/Contents.swift
+++ Dynamic Table/Dynamic Table/Playgrounds/Views.playground/Contents.swift
@@ -8,25 +8,50 @@ let style: JSON = [
     "title": [ "size": 16]
 ]
 
-let json: JSON = [
-    "title": "Hello!",
-    "text": "Product A",
+let json: [JSON] = [
+    ["title": "Hello!"],
+    ["text": "Product A"],
 ]
 
-
+final class TextCell: UICollectionViewCell {
+    
+    private let label = UILabel()
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUp() {
+        addSubview(label)
+    }
+    
+    func configure(with text: String) {
+        label.text = text
+    }
+}
 
 final class ViewController: UIViewController {
+    
+    private let loadedJSON: [JSON] = json
     
     private var collectionView: UICollectionView {
         didSet {
             collectionView.delegate = self
             collectionView.dataSource = self
+            collectionView.backgroundColor = .white
+            collectionView.register(TextCell.self, forCellWithReuseIdentifier: "textCell")
         }
     }
     
     init() {
         self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
         super.init(nibName: nil, bundle: nil)
+        setUp(view)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -36,16 +61,27 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    private func setUp(_ view: UIView) {
+        view.addSubview(collectionView)
+    }
 }
 
 extension ViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return loadedJSON.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        let data = loadedJSON[indexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "textCell", for: indexPath) as! TextCell
+        
+        if let text = data["text"] as? String {
+            cell.configure(with: text)
+        }
+        
+        return cell
     }
     
 }
